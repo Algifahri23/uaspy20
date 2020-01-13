ef79306ebbca4125df6a70bf1b32ea9aa3ef38fd
Algifahri23 <59810955+Algifahri23@users.noreply.github.com>
2020-01-13 09:46:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,29 @@ class MainApp():
 if __name__ == "__main__":
     app = MainApp()
     app.run()
+class MainApp(BaseApp):
+    def _init_(self):
+        self.books = []
+        self.InputBook = []
+        self.ViewBook = []
+        BaseApp._init_(self)
 
+
+class ViewBook(Book):
+    def _init_(self):
+        vBook = ViewBook (self, books)
+        vBook.list_book()
+
+
+    if _name_ == "_main_":
+        app = MainApp(BaseApp)
+        app.run()
+
+
+    @property
+    def list_book(self):
+        return self.list_book
+    def add_book(self):
+        return self.add_book()
+    def search_book(self):
+        return self.search_book()
